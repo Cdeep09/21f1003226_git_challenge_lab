82708e35975b4112bd5cb734d3036e9e8c2276ea
Cdeep09 <livetolearn512@gmail.com>
2025-09-17 14:54:24 +0530

    Added main.py with basic arithmetic operations.

---
+++ main.py
@@ -0,0 +1,8 @@
+def add(a, b): return a + b
+def sub(a, b): return a - b
+def mul(a, b): return a * b
+
+if __name__ == "__main__":
+    print("Add:", add(3, 2))
+    print("Sub:", sub(3, 2))
+    print("Mul:", mul(3, 2))
